fc3efd656d399e8571e83a2a7c531e86ddc8dd98
TiDJ <tomjamon0726@gmail.com>
2017-04-04 17:20:36 +0200

    Add WAMP PATH and VHOST PATH (Defaut WAMP 64 v3, mostly used)

--- config.php
+++ config.php
@@ -14,4 +14,7 @@ define("WWW_DIR", "../");
 // Ignored projects to not sync
 define("IGNORED_PROJECTS", 'n-a,.,..,index,css,.git,js,img,damp');
 
+define('WAMP_PATH', realpath('C:/wamp64').'\\');
+define('CUSTOM_VHOST_PATH_CONFIG', "C:/wamp64/bin/apache/apache*/conf/extra/httpd-vhosts.conf");
+
 ?>
